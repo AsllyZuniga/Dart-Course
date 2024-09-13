3b634aa1c49ffe0081653001599777d2ed279f38
Aslly Zuñiga <asllyz2610@gmail.com>
2024-09-12 21:00:06 -0500

    add folder 02 control to flow control section

---
+++ ejercicios/02_control/01_obtener_input.dart
@@ -0,0 +1,12 @@
+import 'dart:io';
+
+main(){
+  //Imprimir en terminal o en cmd
+  stdout.write('¿Cuál es tu nombre?');
+
+  //Leer información
+  String? nombre = stdin.readLineSync(); 
+
+  stdout.write('Tu nombre es: $nombre');
+
+}
---
+++ ejercicios/02_control/03_for.dart
@@ -0,0 +1,19 @@
+import 'dart:io';
+void main() {
+  for (int i = 0; i < 10; i++) {
+    print(i);  // Ejemplo: imprimir los números del 0 al 9
+  }
+
+  /*
+  Dato de entrada: La base de la tabla de multiplicar 
+  (ese dato debe ser capturado por el usuario)
+  */
+  stdout.writeln('¿Cuál es la base de la tabla?');
+  int base = int.parse( stdin.readLineSync() );
+
+  for (var i = 1; i <= 10; i++) {
+    stdout.writeln('2 * $i = ${i * base}');
+
+    
+  }
+}
