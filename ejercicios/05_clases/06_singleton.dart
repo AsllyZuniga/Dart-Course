310ab1f5ecdee867d72aec12872f4ca17a2c59ed
Aslly Zuñiga <asllyz2610@gmail.com>
2024-10-08 18:52:34 -0500

    Update branch with class exercises

---
+++ ejercicios/05_clases/06_singleton.dart
@@ -0,0 +1,10 @@
+import 'clases/mi_servicio.dart';
+
+void main(List<String> args) {
+  
+  final spotifyService = new MiServicio();
+  spotifyService.url = 'https//api.spotify.com';
+
+   final spotifyService2 = new MiServicio();
+  spotifyService.url = 'https//api.spotify.com';
+}
